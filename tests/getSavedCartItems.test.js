da2af37baadb74569d013bf0b789bd5f516eb3b1
Mayara Marques <mayaramarquescosta@outlook.com>
2022-07-13 14:14:15 -0300

    Requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,9 +1,16 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
+const saveCartItems = require('../helpers/saveCartItems');
 
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se a função retorna o localStorage.setItem', () => {  
+    getSavedCartItems();
+    expect(localStorage.getItem).toReturn();
+  })
+  it('Verifica se ao executar getSavedCartItems, o método localStorage.getItem é chamado com o cartItems como parâmetro', () => {
+    getSavedCartItems('cartItems');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });
